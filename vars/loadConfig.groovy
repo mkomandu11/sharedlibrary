8d536e7959cd14e8cc8b3554f51404b35d9ccd43
mkomandu11 <madhuri.1105@gmail.com>
2024-11-12 09:47:29 +0530

    Update loadConfig.groovy

--- vars/loadConfig.groovy
+++ vars/loadConfig.groovy
@@ -1,6 +1,22 @@
 // vars/loadConfig.groovy
-def call(String filePath = 'config.yml') {
-    def yamlContent = libraryResource(filePath)
+def call(String keyPath = null) {
+    // Load the YAML file content from the hardcoded path
+    def yamlContent = libraryResource('config.yaml')
     def config = readYaml text: yamlContent
+
+    // If a specific keyPath is requested, navigate the config map
+    if (keyPath) {
+        def keys = keyPath.split('\\.')
+        def value = config
+        for (key in keys) {
+            value = value[key]
+            if (value == null) {
+                error "Key path '${keyPath}' not found in configuration."
+            }
+        }
+        return value
+    }
+
+    // Return the entire config if no specific keyPath is provided
     return config
 }
