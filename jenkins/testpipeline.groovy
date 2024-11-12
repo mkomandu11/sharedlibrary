5f0e610b08e3b8f215d14ead76cd2279d1c1f990
mkomandu11 <madhuri.1105@gmail.com>
2024-11-12 10:23:29 +0530

    Update testpipeline.groovy

--- jenkins/testpipeline.groovy
+++ jenkins/testpipeline.groovy
@@ -7,9 +7,11 @@ pipeline {
                 script {
                     def environmentName = loadConfig('environment.name')
                     def databaseHost = loadConfig('database.host')
+                    def hosts = loadConfig('HOSTS')
                     
                     echo "Environment Name: ${environmentName}"
                     echo "Database Host: ${databaseHost}"
+                     echo "Host: ${hosts}"
                 }
             }
         }
