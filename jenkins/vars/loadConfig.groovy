55bd1990fa736d1202573b54ca13a371dfea510f
mkomandu11 <madhuri.1105@gmail.com>
2024-11-12 10:34:59 +0530

    Update loadConfig.groovy

--- jenkins/vars/loadConfig.groovy
+++ jenkins/vars/loadConfig.groovy
@@ -1,22 +1,44 @@
 // vars/loadConfig.groovy
-def call(String keyPath = null) {
-    // Load the YAML file content from the hardcoded path
-    def yamlContent = libraryResource('config.yaml')
-    def config = readYaml text: yamlContent
+def configCache = null
 
-    // If a specific keyPath is requested, navigate the config map
-    if (keyPath) {
-        def keys = keyPath.split('\\.')
-        def value = config
-        for (key in keys) {
-            value = value[key]
-            if (value == null) {
-                error "Key path '${keyPath}' not found in configuration."
-            }
-        }
-        return value
+// Static method to load the YAML content (only once)
+static def loadYamlContent() {
+    if (configCache == null) {
+        def yamlContent = libraryResource('config.yml')
+        configCache = readYaml text: yamlContent
     }
+    return configCache
+}
 
-    // Return the entire config if no specific keyPath is provided
-    return config
+// Static method to get specific key values by keyPath
+static def getConfigValue(String keyPath) {
+    def config = loadYamlContent()
+    def keys = keyPath.split('\\.')
+    def value = config
+    for (key in keys) {
+        value = value[key]
+        if (value == null) {
+            error "Key path '${keyPath}' not found in configuration."
+        }
+    }
+    return value
+}
+
+// Static method to get the 'HOSTS' as a single string
+static def getHosts() {
+    def hosts = getConfigValue('HOSTS')
+    if (hosts instanceof List) {
+        return hosts.join(', ')
+    }
+    error "HOSTS is not a list in the configuration."
+}
+
+// Static method to get the 'environment.name'
+static def getEnvironmentName() {
+    return getConfigValue('environment.name')
+}
+
+// Static method to get the 'database.host'
+static def getDatabaseHost() {
+    return getConfigValue('database.host')
 }
